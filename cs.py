4fc4903ba4ccd6b19bb075a11740f56de0abc4cd
Michael Constantine Dimopoulos <31562759+MichaelDim02@users.noreply.github.com>
2019-03-02 11:02:47 +0000

    Update cs.py

--- cs.py
+++ cs.py
@@ -2,37 +2,133 @@ import webbrowser
 import os
 
 def email():
+	print "Email Services:\n"
+	print "	1. CyberSpy"
+	print "\n	Third Party Websites:"
+	print "	2. OSINT BOOKMARKLETS v0.3"
+	print "	3. com.lullar.com"
+	print "	4. email-checker.net"
+	print "	5. EmailFinder"
+	print "\n	x. Back to menu\n"
 	try:
-		print "Email Services:\n"
-		print "	1. CyberSpy"
-
-		print "\n	Third Party Websites:"
-		print "	2. OSINT BOOKMARKLETS v0.3"
-		print "	3. Lullar.com"
-
-		print "	4. EmailFinder"
-
-		print "	x. Back to menu"
 		option2 = raw_input(">> ")
-		if option2 == "1":
-			link = os.path.abspath("files/email.html")
-			webbrowser.open(link, new=2)
-		elif option2 == "2":
-			link = os.path.abspath("files/osintemail.html")
-			webbrowser.open(link, new=2)
-		elif option2 == "3":
-			webbrowser.open("http://com.lullar.com/", new=2)
-		elif option2 == "4":
-			webbrowser.open("https://www.emailfinder.com/", new=2)
-		else:
-			banner()
-
 	except KeyboardInterrupt:
 		print "\nBye!"
 		exit()
-def username():
- print "Hi" 
+	if option2 == "1":
+		link = os.path.abspath("files/email.html")
+		webbrowser.open(link, new=2)
+	elif option2 == "2":
+		link = os.path.abspath("files/osintemail.html")
+		webbrowser.open(link, new=2)
+	elif option2 == "3":
+		webbrowser.open("http://com.lullar.com/", new=2)
+	elif option2 == "4":
+		webbrowser.open("https://email-checker.net/", new=2)
+	elif option2 == "5":
+		webbrowser.open("https://www.emailfinder.com/", new=2)
+	else:
+		banner()
 
+def username():
+	print "Username Services:\n"
+	print "	1. CyberSpy"
+	print "	Third Party Websites:"
+	print "	2. OSINT BOOKMARKLETS v0.3"
+	print "	3. com.lullar.com"
+	print "	4. Google Advanced Search"
+	print "	5. Pipl.com"
+	print "	6. WebMii"
+	print "	7. PeekYou"
+	print "\n	x. Back to menu\n"
+	try:
+		option2 = raw_input(">> ")
+	except KeyboardInterrupt:
+		print "\nBye!"
+		exit()
+	if option2 == "1":
+		link = os.path.abspath("files/username.html")
+		webbrowser.open(link, new=2)
+	elif option2 == "2":
+		link = os.path.abspath("files/osintusername.html")
+		webbrowser.open(link, new=2)
+	elif option2 == "3":
+		webbrowser.open("http://com.lullar.com/", new=2)
+	elif option2 == "4":
+		webbrowser.open("https://www.google.com/advanced_search", new=2)
+	elif option2 == "5":
+		webbrowser.open("http://www.pipl.com", new=2)
+	elif option2 == "6":
+		webbrowser.open("http://webmii.com/", new=2) #legal names too
+	elif option2 == "7":
+		webbrowser.open("https://www.peekyou.com/username", new=2) #legal names too
+	else:
+		banner()
+
+def legalname():
+	print "Legal Name Services:\n"
+	print "	Third Party Websites:"
+	print "	1. White Pages"
+	print "	2. OSINT BOOKMARKLETS v0.3"
+	print "	3. com.lullar.com"
+	print "	4. ZabaSearch (US)"
+	print "	5. Pipl.com"
+	print "	6. WebMii"
+	print "	7. PeekYou"
+	print "	8. 411.com"	
+	print "	9. Public Records"
+	print "\n	x. Back to menu\n"
+	try:
+		option2 = raw_input(">> ")
+	except KeyboardInterrupt:
+		print "\nBye!"
+		exit()
+	if option2 == "1":
+		webbrowser.open("https://whitepages.plus/", new=2)
+	elif option2 == "2":
+		webbrowser.open("", new=2)
+	elif option2 == "3":
+		webbrowser.open("http://com.lullar.com/", new=2)
+	elif option2 == "4":
+		webbrowser.open("https://www.google.com/advanced_search", new=2)
+	elif option2 == "5":
+		webbrowser.open("http://www.pipl.com", new=2)
+	elif option2 == "6":
+		webbrowser.open("http://webmii.com/", new=2)
+	elif option2 == "7":
+		webbrowser.open("https://www.peekyou.com/", new=2)
+	elif option2 == "8":
+		webbrowser.open("https://www.411.com/", new=2)
+	elif option2 == "9":
+		webbrowser.open("https://publicrecords.directory/", new=2)
+	else:
+		banner()
+
+def number():
+	print "Phone number Services:\n"
+	print "	Third Party Websites:"
+	print "	1. PeekYou"
+	print "	3. OSINT BOOKMARKLETS v0.3"
+	print "	4. ZabaSearch"
+	print "\n	x. Back to menu\n"
+	try:
+		option2 = raw_input(">> ")
+	except KeyboardInterrupt:
+		print "\nBye!"
+		exit()
+	if option2 == "1":
+		webbrowser.open("https://www.peekyou.com/phone", new=2)
+	elif option2 == "2":
+		link = os.path.abspath("files/osintphone.html")
+		webbrowser.open(link, new=2)
+	elif option2 == "3":
+		webbrowser.open("https://www.zabasearch.com/reverse-phone-lookup/", new=2)
+	elif option2 == "4":
+		webbrowser.open("", new=2)
+	elif option2 == "5":
+		webbrowser.open("", new=2)
+	else:
+		banner()
 def banner():
 	print """
                 .------.....______
@@ -74,11 +170,12 @@ Services:
 	2. Username Search
 	3. Legal name Search
 	4. Phone number Search
-	5. IP Address Seach
-	6. Reverse Image Search
-	7. Image Data Extraction
-	8. ZIP Code Search
-	9. Make a report
+	5. IP Address services
+	6. Image services
+	7. ZIP Code Search
+	8. Make a report
+	9. Other Websites
+	0. GeoLocation
 	x. Exit CyberSpy
 """
 	try:
@@ -90,6 +187,10 @@ Services:
 		email()
 	elif option == "2":
 		username()
+	elif option == "3":
+		legalname()
+	elif option == "4":
+		number()
 	else:
 		exit()
 
